1b7f7f96e0d7683467c31a58e84e7ab4b9fc6e39
Nisanath <91584469+Nisanath@users.noreply.github.com>
2022-12-01 19:54:27 -0800

    Add files via upload
    code with lcd, pump and sensor

---
+++ code/workingcode.ino
@@ -0,0 +1,62 @@
+#include <LiquidCrystal_I2C.h>
+
+const int MOTOR_EN = A0; //pin for the pump
+const int MOIST_IN = A1; //pin for the sensor
+const int MAX_ANALOG_VAL = 4095;
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();      // Make sure backlight is on
+
+  pinMode(MOTOR_EN,OUTPUT);
+  pinMode(MOIST_IN,INPUT);
+  digitalWrite(MOTOR_EN, HIGH);
+  delay(5000);
+  
+}
+
+void loop(){
+
+
+  // put your main code here, to run repeatedly:
+  int moistVal = analogRead(MOIST_IN);
+  //moistVal = map(moistVal, 550,10,0,100);
+  Serial.print("\nMoisture level: \n"); 
+  Serial.print(moistVal);
+
+  if(moistVal > 287){
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("Watering.. ");
+  lcd.setCursor(2,1);
+  lcd.print(moistVal);
+  
+  digitalWrite(MOTOR_EN,LOW);
+  delay(5000); // water 5seconds
+  lcd.clear();         
+  digitalWrite(MOTOR_EN,HIGH);
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print("Done");
+
+  }
+else{
+  lcd.clear();
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print("Soil is dry");
+  delay(5000);
+  lcd.clear();
+  delay(1000);
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print(moistVal);
+  }
+
+  delay(2000);
+}
